7b1b77199c89168430702f4b389456744fc6d798
ash0419 <58350889+ash0419@users.noreply.github.com>
2020-10-23 19:48:11 +0900

    Add files via upload

---
+++ src/CollectionsFrameWorks/StackQueueEx.java
@@ -0,0 +1,41 @@
+package CollectionsFrameWorks;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class StackQueueEx {
+	public static void main(String[] args) {
+		Stack st = new Stack();
+		Queue q = new LinkedList();
+		
+		st.push("0");
+		st.push("1");
+		st.push("2");
+		
+		System.out.println(st);
+		
+		System.out.println("= stack ");
+		while(!st.empty())
+		{
+			System.out.println(st.pop());
+		}
+		
+		System.out.println(st);
+		
+		
+		q.offer("0");
+		q.offer("1");
+		q.offer("2");
+		
+		System.out.println(q);
+		
+		System.out.println("= Queue ");
+		while(!q.isEmpty())
+		{
+			System.out.println(q.poll());
+		}
+		
+		System.out.println(q);
+	}
+}
