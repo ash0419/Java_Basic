ca96300b7114dd4ba8e97225d92d7f6f7fb2f1b9
ash0419 <58350889+ash0419@users.noreply.github.com>
2020-10-30 18:08:00 +0900

    Add files via upload

--- src/ThreadEx/Ex1029_2.java
+++ src/ThreadEx/Ex1029_2.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 class User extends Thread {
 	int sum;
+
 	public User(String user) {
 		super(user);
 	}
@@ -11,9 +12,9 @@ class User extends Thread {
 	public void run() {
 		sum = 0;
 		for (int i = 0; i <= 100; i++) {
-			Random r =new Random();
+			Random r = new Random();
 			int num = r.nextInt(20) + 1;
-			System.out.println(getName() +" : " +num);
+			System.out.println(getName() + " : " + num);
 			sum += num;
 		}
 	}
@@ -25,7 +26,7 @@ public class Ex1029_2 {
 		User user2 = new User("User2");
 		user1.start();
 		user2.start();
-		
+
 		try {
 			user1.join();
 			user2.join();
@@ -33,14 +34,14 @@ public class Ex1029_2 {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("User1의 값 : " +user1.sum);
-		System.out.println("User2의 값 : " +user2.sum);
-		
+		System.out.println("User1의 값 : " + user1.sum);
+		System.out.println("User2의 값 : " + user2.sum);
+
 		if (user1.sum > user2.sum) {
 			System.out.println("User 1의 합이 더 큽니다.");
-		}else if (user1.sum < user2.sum) {
+		} else if (user1.sum < user2.sum) {
 			System.out.println("User 2의 합이 더 큽니다.");
-		}else {
+		} else {
 			System.out.println("합이 똑같습니다.");
 		}
 	}
